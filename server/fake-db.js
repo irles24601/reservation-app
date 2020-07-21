ca8b3653db7cfe6c1f20374b3cad9a1e888ea9fc
Toshiki Isawa <jack_the_fool@outlook.com>
2020-07-21 20:56:22 +0900

    MongoDB
    UnhandledPromiseRejectionWarning

---
+++ server/fake-db.js
@@ -0,0 +1,72 @@
+const Product = require('./model/product')
+
+class FakeDb {
+
+  constructor() {
+    this.products = [
+      {
+        coverImage: '/assets/img/phone-cover.jpg',
+        name: 'Phone XL',
+        price: 799,
+        description: 'A large phone with one of the best screens',
+        heading1: 'サンプルテキスト１',
+        heading2: 'サンプルテキスト２',
+        heading3: 'サンプルテキスト３',
+        headingtext1: '欧州連合（ＥＵ）はブリュッセルで17日に開幕した首脳会議を延長し、新型コロナウイルスで打撃を被る国々を支援する「復興基金」について20日未明まで集中的に協議した。',
+        headingtext2: '事情に詳しい複数の当局者によれば、実質的な復興基金となる総額7500億ユーロ（約92兆円）の経済再建策のうち、返済が必要ない補助金が占める割合を3900億ユーロとする最新案が夜を徹して協議された。',
+        headingtext3: '協議の非公開を理由に当局者２人が匿名を条件に語ったところでは、補助金額に異論を唱えていたオランダとオーストリア、デンマーク、スウェーデンの４カ国が補助金部分を3900億ユーロとする案に満足し、妥協案を受け入れる用意がある。'
+      },
+      {
+        coverImage: '/assets/img/phone-cover.jpg',
+        name: 'Phone Mini',
+        price: 699,
+        description: 'A great phone with one of the best cameras',
+        heading1: 'サンプルテキスト１',
+        heading2: 'サンプルテキスト２',
+        heading3: 'サンプルテキスト３',
+        headingtext1: 'サンプルテキスト　サンプルテキスト　サンプルテキスト　サンプルテキスト　サンプルテキスト',
+        headingtext2: 'テキスト文章　テキスト文章　テキスト文章　テキスト文章　テキスト文章　テキスト文章　テキスト文章　',
+        headingtext3: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.'
+      },
+      {
+        coverImage: '/assets/img/phone-cover.jpg',
+        name: 'Phone Standard',
+        price: 299,
+        description: '',
+        heading1: 'サンプルテキスト１',
+        heading2: 'サンプルテキスト２',
+        heading3: 'サンプルテキスト３',
+        headingtext1: 'サンプルテキスト　サンプルテキスト　サンプルテキスト　サンプルテキスト　サンプルテキスト',
+        headingtext2: 'テキスト文章　テキスト文章　テキスト文章　テキスト文章　テキスト文章　テキスト文章　テキスト文章　',
+        headingtext3: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.'
+      },
+      {
+        coverImage: '/assets/img/phone-cover.jpg',
+        name: 'Phone Special',
+        price: 999,
+        description: '',
+        heading1: 'サンプルテキスト１',
+        heading2: 'サンプルテキスト２',
+        heading3: 'サンプルテキスト３',
+        headingtext1: 'サンプルテキスト　サンプルテキスト　サンプルテキスト　サンプルテキスト　サンプルテキスト',
+        headingtext2: 'テキスト文章　テキスト文章　テキスト文章　テキスト文章　テキスト文章　テキスト文章　テキスト文章　',
+        headingtext3: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.'
+      }    
+    ]
+  }
+
+  pushProductsToDb() {
+    this.products.forEach(
+      (product) => {
+        const newProduct = new Product(product)
+        newProduct.save
+      }
+    )
+  }
+
+  seeDb() {
+    this.pushProductsToDb()
+  }
+}
+
+module.exports = FakeDb
